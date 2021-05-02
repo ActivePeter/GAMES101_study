ad23c2efe27594f34c6c323cbc719366155a913a
ActivePeter <1020401660@qq.com>
2021-05-03 02:22:46 +0800

    base setted

---
+++ pa0/src/transforms/base.cpp
@@ -0,0 +1,5 @@
+#include "base.h"
+
+void rotate_2f(Eigen::Matrix3f point, float eular_angle)
+{
+}
---
+++ pa0/src/transforms/base.h
@@ -0,0 +1,3 @@
+#pragma once
+#include "Eigen/Core"
+void rotate_2f(Eigen::Matrix3f point, float eular_angle);
